2b7ecf144abf178fad605d3adcedec93b695d05e
David S <david.smith216@yahoo.com>
2020-09-05 10:16:00 -0500

    Update Answer Forms

--- src/containers/NewQuestion.js
+++ src/containers/NewQuestion.js
@@ -13,20 +13,31 @@ import AnswerFields from "../components/AnswerFields";
 import QuestionUnit from "../data/questionUnit";
 import QuestionTopic from "../data/questionTopic";
 
-
 export default function NewQuestion() {
   const file = useRef(null);
   const history = useHistory();
   const [questionStatus, setQuestionStatus] = useState("Incomplete");
   const [questionStatement, setQuestionStatement] = useState("");
   //const [answerCorrect1, setAnswerCorrect1] = useState("");
-  //const [mcOption1, setMcOption1] = useState("");
-  //const [mcOption1Feedback, setMcOption1Feedback] = useState("");
+  const [mcOption1, setMcOption1] = useState("");
+  const [mcOption1Feedback, setMcOption1Feedback] = useState("");
+  const [mcCorrectAnswer1, setMcCorrectAnswer1] = useState(false);
+  const [mcOption2, setMcOption2] = useState("");
+  const [mcOption2Feedback, setMcOption2Feedback] = useState("");
+  const [mcCorrectAnswer2, setMcCorrectAnswer2] = useState(false);
+  const [mcOption3, setMcOption3] = useState("");
+  const [mcOption3Feedback, setMcOption3Feedback] = useState("");
+  const [mcCorrectAnswer3, setMcCorrectAnswer3] = useState(false);
+  const [mcOption4, setMcOption4] = useState("");
+  const [mcOption4Feedback, setMcOption4Feedback] = useState("");
+  const [mcCorrectAnswer4, setMcCorrectAnswer4] = useState(false);
+  const [mcOption5, setMcOption5] = useState("");
+  const [mcOption5Feedback, setMcOption5Feedback] = useState("");
+  const [mcCorrectAnswer5, setMcCorrectAnswer5] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const questionType = "Multiple-Choice";
   const [questionUnit, setQuestionUnit] = useState("");
   const [questionTopic, setQuestionTopic] = useState("");
-
 
   // handle onChange event of the Status dropdown
   const handleStatusChange = (e) => {
@@ -73,6 +84,21 @@ export default function NewQuestion() {
         questionType,
         questionUnit,
         questionTopic,
+        mcOption1,
+        mcOption1Feedback,
+        mcCorrectAnswer1,
+        mcOption2,
+        mcOption2Feedback,
+        mcCorrectAnswer2,
+        mcOption3,
+        mcOption3Feedback,
+        mcCorrectAnswer3,
+        mcOption4,
+        mcOption4Feedback,
+        mcCorrectAnswer4,
+        mcOption5,
+        mcOption5Feedback,
+        mcCorrectAnswer5,
       });
       history.push("/");
     } catch (e) {
@@ -90,33 +116,33 @@ export default function NewQuestion() {
   return (
     <div className="NewQuestion">
       <form onSubmit={handleSubmit}>
-      <FormGroup controlId="questionUnit">
-            <ControlLabel>Unit</ControlLabel>
-            <Select
-              className="basic-single"
-              classNamePrefix="select"
-              placeholder="Set the Unit"
-              value={QuestionUnit.find((obj) => obj.value === questionUnit)}
-              onChange={handleUnitChange}
-              isSearchable="true"
-              name="questionUnit"
-              options={QuestionUnit}
-            />
-          </FormGroup>
+        <FormGroup controlId="questionUnit">
+          <ControlLabel>Unit</ControlLabel>
+          <Select
+            className="basic-single"
+            classNamePrefix="select"
+            placeholder="Set the Unit"
+            value={QuestionUnit.find((obj) => obj.value === questionUnit)}
+            onChange={handleUnitChange}
+            isSearchable="true"
+            name="questionUnit"
+            options={QuestionUnit}
+          />
+        </FormGroup>
 
-          <FormGroup controlId="questionTopic">
-            <ControlLabel>Topic</ControlLabel>
-            <Select
-              className="basic-single"
-              classNamePrefix="select"
-              placeholder="Set the topic"
-              value={QuestionTopic.find((obj) => obj.value === questionTopic)}
-              onChange={handleTopicChange}
-              isSearchable="true"
-              name="questionTopic"
-              options={QuestionTopic}
-            />
-          </FormGroup>
+        <FormGroup controlId="questionTopic">
+          <ControlLabel>Topic</ControlLabel>
+          <Select
+            className="basic-single"
+            classNamePrefix="select"
+            placeholder="Set the topic"
+            value={QuestionTopic.find((obj) => obj.value === questionTopic)}
+            onChange={handleTopicChange}
+            isSearchable="true"
+            name="questionTopic"
+            options={QuestionTopic}
+          />
+        </FormGroup>
 
         <FormGroup controlId="questionStatus">
           <ControlLabel>Question Status</ControlLabel>
@@ -144,13 +170,169 @@ export default function NewQuestion() {
           <ControlLabel>Attachment</ControlLabel>
           <FormControl onChange={handleFileChange} type="file" />
         </FormGroup>
-        <FormGroup controlId="AnswerGroup">
-          <ControlLabel for="Answers">Answers and Feedback</ControlLabel>
-          <AnswerFields
-            id="Answers"
-            onChange={(e) => setQuestionStatement(e.target.value)}
+
+        <FormGroup controlId="AnswerGroup1">
+          <dev>
+            <ControlLabel for="Answers">Answer Set #1</ControlLabel>
+          </dev>
+          <dev>
+            <FormControl
+              value={mcCorrectAnswer1}
+              id="CorrectAnswer"
+              type="checkbox"
+            />
+          </dev>
+          <ControlLabel for="Answer">Answer</ControlLabel>
+          <FormControl
+            value={mcOption1}
+            id="Answer"
+            class="answerfield"
+            componentClass="textarea"
+            onChange={(e) => setMcOption1(e.target.value)}
           />
+          <ControlLabel for="Answerfeedback">Feedback</ControlLabel>
+          <FormControl
+            value={mcOption1Feedback}
+            id="Answerfeedback"
+            componentClass="textarea"
+            onChange={(e) => setMcOption1Feedback(e.target.value)}
+          />
+          <FormGroup controlId="file">
+            <ControlLabel>Attachment</ControlLabel>
+            <FormControl onChange={handleFileChange} type="file" />
+          </FormGroup>
         </FormGroup>
+
+        <FormGroup controlId="AnswerGroup2">
+          <dev>
+            <ControlLabel for="Answers">Answer Set #2</ControlLabel>
+          </dev>
+          <dev>
+            <FormControl
+              value={mcCorrectAnswer2}
+              id="CorrectAnswer"
+              type="checkbox"
+            />
+          </dev>
+          <ControlLabel for="Answer">Answer</ControlLabel>
+          <FormControl
+            value={mcOption2}
+            id="Answer"
+            class="answerfield"
+            componentClass="textarea"
+            onChange={(e) => setMcOption2(e.target.value)}
+          />
+          <ControlLabel for="Answerfeedback">Feedback</ControlLabel>
+          <FormControl
+            value={mcOption2Feedback}
+            id="Answerfeedback"
+            componentClass="textarea"
+            onChange={(e) => setMcOption2Feedback(e.target.value)}
+          />
+
+          <FormGroup controlId="file">
+            <ControlLabel>Attachment</ControlLabel>
+            <FormControl onChange={handleFileChange} type="file" />
+          </FormGroup>
+        </FormGroup>
+
+        <FormGroup controlId="AnswerGroup3">
+          <dev>
+            <ControlLabel for="Answers">Answer Set #3</ControlLabel>
+          </dev>
+          <dev>
+            <FormControl
+              value={mcCorrectAnswer3}
+              id="CorrectAnswer"
+              type="checkbox"
+            />
+          </dev>
+          <ControlLabel for="Answer">Answer</ControlLabel>
+          <FormControl
+            value={mcOption3}
+            id="Answer"
+            class="answerfield"
+            componentClass="textarea"
+            onChange={(e) => setMcOption3(e.target.value)}
+          />
+          <ControlLabel for="Answerfeedback">Feedback</ControlLabel>
+          <FormControl
+            value={mcOption3Feedback}
+            id="Answerfeedback"
+            componentClass="textarea"
+            onChange={(e) => setMcOption3Feedback(e.target.value)}
+          />
+          <FormGroup controlId="file">
+            <ControlLabel>Attachment</ControlLabel>
+            <FormControl onChange={handleFileChange} type="file" />
+          </FormGroup>
+        </FormGroup>
+
+        <FormGroup controlId="AnswerGroup4">
+          <dev>
+            <ControlLabel for="Answers">Answer Set #4</ControlLabel>
+          </dev>
+          <dev>
+            <FormControl
+              value={mcCorrectAnswer4}
+              id="CorrectAnswer"
+              type="checkbox"
+            />
+          </dev>
+          <ControlLabel for="Answer">Answer</ControlLabel>
+          <FormControl
+            value={mcOption4}
+            id="Answer"
+            class="answerfield"
+            componentClass="textarea"
+            onChange={(e) => setMcOption4(e.target.value)}
+          />
+          <ControlLabel for="Answerfeedback">Feedback</ControlLabel>
+          <FormControl
+            value={mcOption4Feedback}
+            id="Answerfeedback"
+            componentClass="textarea"
+            onChange={(e) => setMcOption4Feedback(e.target.value)}
+          />
+          <FormGroup controlId="file">
+            <ControlLabel>Attachment</ControlLabel>
+            <FormControl onChange={handleFileChange} type="file" />
+          </FormGroup>
+        </FormGroup>
+
+        <FormGroup controlId="AnswerGroup5">
+          <dev>
+            <ControlLabel for="Answers">Answer Set #5</ControlLabel>
+          </dev>
+          <dev>
+            <FormControl
+              value={mcCorrectAnswer5}
+              id="CorrectAnswer"
+              type="checkbox"
+            />
+          </dev>
+          <ControlLabel for="Answer">Answer</ControlLabel>
+          <FormControl
+            value={mcOption5}
+            id="Answer"
+            class="answerfield"
+            componentClass="textarea"
+            onChange={(e) => setMcOption5(e.target.value)}
+          />
+          <ControlLabel for="Answerfeedback">Feedback</ControlLabel>
+          <FormControl
+            value={mcOption5Feedback}
+            id="Answerfeedback"
+            componentClass="textarea"
+            onChange={(e) => setMcOption5Feedback(e.target.value)}
+          />
+
+          <FormGroup controlId="file">
+            <ControlLabel>Attachment</ControlLabel>
+            <FormControl onChange={handleFileChange} type="file" />
+          </FormGroup>
+        </FormGroup>
+
         <LoaderButton
           block
           type="submit"
--- src/containers/Questions.js
+++ src/containers/Questions.js
@@ -9,12 +9,12 @@ import "./Questions.css";
 import { s3Upload } from "../libs/awsLib";
 import Select from "react-select";
 import Options from "../data/statusOptions";
-import AnswerFields from "../components/AnswerFields";
 import QuestionUnit from "../data/questionUnit";
 import QuestionTopic from "../data/questionTopic";
 
 export default function Questions() {
   const file = useRef(null);
+  const feedbackfile1 = useRef(null);
   const { id } = useParams();
   const history = useHistory();
   //    const [questionType, setQuestionType] = useState("Multiple-Choice"); // Probably remove before production
@@ -26,6 +26,21 @@ export default function Questions() {
   const [isDeleting, setIsDeleting] = useState(false);
   const [questionUnit, setQuestionUnit] = useState("");
   const [questionTopic, setQuestionTopic] = useState("");
+  const [mcOption1, setMcOption1] = useState("");
+  const [mcOption1Feedback, setMcOption1Feedback] = useState("");
+  const [mcCorrectAnswer1, setMcCorrectAnswer1] = useState(false);
+  const [mcOption2, setMcOption2] = useState("");
+  const [mcOption2Feedback, setMcOption2Feedback] = useState("");
+  const [mcCorrectAnswer2, setMcCorrectAnswer2] = useState(false);
+  const [mcOption3, setMcOption3] = useState("");
+  const [mcOption3Feedback, setMcOption3Feedback] = useState("");
+  const [mcCorrectAnswer3, setMcCorrectAnswer3] = useState(false);
+  const [mcOption4, setMcOption4] = useState("");
+  const [mcOption4Feedback, setMcOption4Feedback] = useState("");
+  const [mcCorrectAnswer4, setMcCorrectAnswer4] = useState(false);
+  const [mcOption5, setMcOption5] = useState("");
+  const [mcOption5Feedback, setMcOption5Feedback] = useState("");
+  const [mcCorrectAnswer5, setMcCorrectAnswer5] = useState(false);
 
   // handle onChange event of the Status dropdown
   const handleStatusChange = (e) => {
@@ -48,7 +63,29 @@ export default function Questions() {
     async function onLoad() {
       try {
         const question = await loadQuestion();
-        const { questionStatement, attachment, questionStatus, questionUnit, questionTopic } = question;
+        const {
+          questionStatement,
+          attachment,
+          mcOption1FeedbackImageName,
+          questionStatus,
+          questionUnit,
+          questionTopic,
+          mcOption1,
+          mcOption1Feedback,
+          mcCorrectAnswer1,
+          mcOption2,
+          mcOption2Feedback,
+          mcCorrectAnswer2,
+          mcOption3,
+          mcOption3Feedback,
+          mcCorrectAnswer3,
+          mcOption4,
+          mcOption4Feedback,
+          mcCorrectAnswer4,
+          mcOption5,
+          mcOption5Feedback,
+          mcCorrectAnswer5,
+        } = question;
 
         if (attachment) {
           question.attachmentURL = await Storage.vault.get(attachment);
@@ -59,6 +96,21 @@ export default function Questions() {
         setQuestionUnit(questionUnit);
         setQuestionTopic(questionTopic);
         setQuestion(question);
+        setMcOption1(mcOption1);
+        setMcOption1Feedback(mcOption1Feedback);
+        setMcCorrectAnswer1(mcCorrectAnswer1);
+        setMcOption2(mcOption2);
+        setMcOption2Feedback(mcOption2Feedback);
+        setMcCorrectAnswer2(mcCorrectAnswer2);
+        setMcOption3(mcOption3);
+        setMcOption3Feedback(mcOption3Feedback);
+        setMcCorrectAnswer3(mcCorrectAnswer3);
+        setMcOption4(mcOption4);
+        setMcOption4Feedback(mcOption4Feedback);
+        setMcCorrectAnswer4(mcCorrectAnswer4);
+        setMcOption5(mcOption5);
+        setMcOption5Feedback(mcOption5Feedback);
+        setMcCorrectAnswer5(mcCorrectAnswer5);
         //setQuestionType(questionType);
       } catch (e) {
         onError(e);
@@ -113,6 +165,21 @@ export default function Questions() {
         questionType,
         questionUnit,
         questionTopic,
+        mcOption1,
+        mcOption1Feedback,
+        mcCorrectAnswer1,
+        mcOption2,
+        mcOption2Feedback,
+        mcCorrectAnswer2,
+        mcOption3,
+        mcOption3Feedback,
+        mcCorrectAnswer3,
+        mcOption4,
+        mcOption4Feedback,
+        mcCorrectAnswer4,
+        mcOption5,
+        mcOption5Feedback,
+        mcCorrectAnswer5,
         attachment: attachment || question.attachment,
       });
       history.push("/");
@@ -220,13 +287,166 @@ export default function Questions() {
             {!question.attachment && <ControlLabel>Attachment</ControlLabel>}
             <FormControl onChange={handleFileChange} type="file" />
           </FormGroup>
-          <FormGroup controlId="AnswerGroup">
-            <ControlLabel for="Answers">Answers and Feedback</ControlLabel>
-            <AnswerFields
-              id="Answers"
-              onChange={(e) => setQuestionStatement(e.target.value)}
+
+          <FormGroup controlId="AnswerGroup1">
+            <dev>
+              <ControlLabel for="Answers">Answer Set #1</ControlLabel>
+            </dev>
+            <dev>
+              <FormControl
+                value={mcCorrectAnswer1}
+                id="CorrectAnswer"
+                type="checkbox"
+              />
+            </dev>
+            <ControlLabel for="Answer">Answer</ControlLabel>
+            <FormControl
+              value={mcOption1}
+              id="Answer"
+              class="answerfield"
+              componentClass="textarea"
+              onChange={(e) => setMcOption1(e.target.value)}
             />
+            <ControlLabel for="Answerfeedback">Feedback</ControlLabel>
+            <FormControl
+              value={mcOption1Feedback}
+              id="Answerfeedback"
+              componentClass="textarea"
+              onChange={(e) => setMcOption1Feedback(e.target.value)}
+            />
+            <FormGroup controlId="file">
+              <ControlLabel>Attachment</ControlLabel>
+              <FormControl onChange={handleFileChange} type="file" />
+            </FormGroup>
           </FormGroup>
+          <FormGroup controlId="AnswerGroup2">
+            <dev>
+              <ControlLabel for="Answers">Answer Set #2</ControlLabel>
+            </dev>
+            <dev>
+              <FormControl
+                value={mcCorrectAnswer2}
+                id="CorrectAnswer"
+                type="checkbox"
+              />
+            </dev>
+            <ControlLabel for="Answer">Answer</ControlLabel>
+            <FormControl
+              value={mcOption2}
+              id="Answer"
+              class="answerfield"
+              componentClass="textarea"
+              onChange={(e) => setMcOption2(e.target.value)}
+            />
+            <ControlLabel for="Answerfeedback">Feedback</ControlLabel>
+            <FormControl
+              value={mcOption2Feedback}
+              id="Answerfeedback"
+              componentClass="textarea"
+              onChange={(e) => setMcOption2Feedback(e.target.value)}
+            />
+            <FormGroup controlId="file">
+              <ControlLabel>Attachment</ControlLabel>
+              <FormControl onChange={handleFileChange} type="file" />
+            </FormGroup>
+          </FormGroup>
+          <FormGroup controlId="AnswerGroup3">
+            <dev>
+              <ControlLabel for="Answers">Answer Set #3</ControlLabel>
+            </dev>
+            <dev>
+              <FormControl
+                value={mcCorrectAnswer3}
+                id="CorrectAnswer"
+                type="checkbox"
+              />
+            </dev>
+            <ControlLabel for="Answer">Answer</ControlLabel>
+            <FormControl
+              value={mcOption3}
+              id="Answer"
+              class="answerfield"
+              componentClass="textarea"
+              onChange={(e) => setMcOption3(e.target.value)}
+            />
+            <ControlLabel for="Answerfeedback">Feedback</ControlLabel>
+            <FormControl
+              value={mcOption3Feedback}
+              id="Answerfeedback"
+              componentClass="textarea"
+              onChange={(e) => setMcOption3Feedback(e.target.value)}
+            />
+            <FormGroup controlId="file">
+              <ControlLabel>Attachment</ControlLabel>
+              <FormControl onChange={handleFileChange} type="file" />
+            </FormGroup>
+          </FormGroup>
+
+          <FormGroup controlId="AnswerGroup4">
+            <dev>
+              <ControlLabel for="Answers">Answer Set #4</ControlLabel>
+            </dev>
+            <dev>
+              <FormControl
+                value={mcCorrectAnswer4}
+                id="CorrectAnswer"
+                type="checkbox"
+              />
+            </dev>
+            <ControlLabel for="Answer">Answer</ControlLabel>
+            <FormControl
+              value={mcOption4}
+              id="Answer"
+              class="answerfield"
+              componentClass="textarea"
+              onChange={(e) => setMcOption4(e.target.value)}
+            />
+            <ControlLabel for="Answerfeedback">Feedback</ControlLabel>
+            <FormControl
+              value={mcOption4Feedback}
+              id="Answerfeedback"
+              componentClass="textarea"
+              onChange={(e) => setMcOption4Feedback(e.target.value)}
+            />
+            <FormGroup controlId="file">
+              <ControlLabel>Attachment</ControlLabel>
+              <FormControl onChange={handleFileChange} type="file" />
+            </FormGroup>
+          </FormGroup>
+
+          <FormGroup controlId="AnswerGroup5">
+            <dev>
+              <ControlLabel for="Answers">Answer Set #5</ControlLabel>
+            </dev>
+            <dev>
+              <FormControl
+                value={mcCorrectAnswer5}
+                id="CorrectAnswer"
+                type="checkbox"
+              />
+            </dev>
+            <ControlLabel for="Answer">Answer</ControlLabel>
+            <FormControl
+              value={mcOption5}
+              id="Answer"
+              class="answerfield"
+              componentClass="textarea"
+              onChange={(e) => setMcOption5(e.target.value)}
+            />
+            <ControlLabel for="Answerfeedback">Feedback</ControlLabel>
+            <FormControl
+              value={mcOption5Feedback}
+              id="Answerfeedback"
+              componentClass="textarea"
+              onChange={(e) => setMcOption5Feedback(e.target.value)}
+            />
+
+            <FormGroup controlId="file">
+              <ControlLabel>Attachment</ControlLabel>
+              <FormControl onChange={handleFileChange} type="file" />
+            </FormGroup>
+          </FormGroup>
+
           <LoaderButton
             block
             type="submit"
