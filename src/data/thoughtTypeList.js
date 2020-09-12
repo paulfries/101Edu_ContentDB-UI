7b9aa2875357ea9b66fce4d7eb262187fcd06a7c
David S <david.smith216@yahoo.com>
2020-09-12 03:13:13 -0500

    Final Revisions for Demo

---
+++ src/data/thoughtTypeList.js
@@ -0,0 +1,7 @@
+const thoughtTypeList = [
+  { value: "Conceptual", label: "Conceptual" },
+  { value: "Quantitative", label: "Quantitative" },
+  { value: "Vocabulary", label: "Vocabulary" },
+];
+
+export default thoughtTypeList;
