7e58cebc044e88e77a2cc45b76cd62f673d09496
Paul Fries <paul@paulfries.com>
2020-10-23 16:20:12 -0700

    Changing home screen label

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -76,7 +76,7 @@ export default function Home() {
   function renderQuestions() {
     return (
       <div className="questions">
-        <PageHeader>My Chem101 Questions</PageHeader>
+        <PageHeader>My Chem101 Question Bank</PageHeader>
         <ListGroup>
           {!isLoading && renderQuestionsList(questions)}
         </ListGroup>
